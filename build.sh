0ea1295622132ca4591e25857445f13ef81805ba
Marek Cirkos <marekcirkos@users.noreply.github.com>
2015-11-23 20:16:41 +0000

    Added iphoneos build to CI

--- build.sh
+++ build.sh
@@ -11,23 +11,19 @@ set -eu
 
 MODE=$1
 
-KEY_CHAIN=ios-build.keychain
-security create-keychain -p travis $KEY_CHAIN
-security default-keychain -s $KEY_CHAIN
-security unlock-keychain -p travis $KEY_CHAIN
-security set-keychain-settings -t 3600 -u $KEY_CHAIN
-
 function ci() {
   xctool \
       -workspace WebDriverAgent.xcworkspace \
       -scheme $1 \
       -sdk $2 \
-      $3
+      $3 \
+      CODE_SIGN_IDENTITY="" \
+      CODE_SIGNING_REQUIRED=NO
 }
 
 if [ "$MODE" = "ci" ]; then
   ci WebDriverAgent iphonesimulator build
   ci XCTUITestRunner iphonesimulator build-tests
-  #ci XCTUITestRunner iphoneos build-tests
+  ci XCTUITestRunner iphoneos build-tests
 fi
 
