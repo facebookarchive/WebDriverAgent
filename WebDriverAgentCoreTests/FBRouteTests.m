1458077e8bba2f8ea7106fdab16c12adcee38d5d
Marek Cirkos <marekc@fb.com>
2015-12-14 11:45:25 +0000

    Properly handling sessions with empty path

--- WebDriverAgentCoreTests/FBRouteTests.m
+++ WebDriverAgentCoreTests/FBRouteTests.m
@@ -17,17 +17,52 @@
 
 @implementation FBRouteTests
 
-- (void)testRouteWithSession
+- (void)testRouteWithSessionWithSlash
 {
   FBRoute *route = [[FBRoute POST:@"/deactivateApp"] respond:nil];
   XCTAssertEqualObjects(route.path, @"/session/:sessionID/deactivateApp");
+}
 
+- (void)testRouteWithSession
+{
+  FBRoute *route = [[FBRoute POST:@"deactivateApp"] respond:nil];
+  XCTAssertEqualObjects(route.path, @"/session/:sessionID/deactivateApp");
+}
+
+- (void)testRouteWithoutSessionWithSlash
+{
+  FBRoute *route = [[FBRoute POST:@"/deactivateApp"].withoutSession respond:nil];
+  XCTAssertEqualObjects(route.path, @"/deactivateApp");
 }
 
 - (void)testRouteWithoutSession
 {
-  FBRoute *route = [[FBRoute POST:@"/deactivateApp"].withoutSession respond:nil];
+  FBRoute *route = [[FBRoute POST:@"deactivateApp"].withoutSession respond:nil];
   XCTAssertEqualObjects(route.path, @"/deactivateApp");
+}
+
+- (void)testEmptyRouteWithSession
+{
+  FBRoute *route = [[FBRoute POST:@""] respond:nil];
+  XCTAssertEqualObjects(route.path, @"/session/:sessionID");
+}
+
+- (void)testEmptyRouteWithoutSession
+{
+  FBRoute *route = [[FBRoute POST:@""].withoutSession respond:nil];
+  XCTAssertEqualObjects(route.path, @"/");
+}
+
+- (void)testEmptyRouteWithSessionWithSlash
+{
+  FBRoute *route = [[FBRoute POST:@"/"] respond:nil];
+  XCTAssertEqualObjects(route.path, @"/session/:sessionID");
+}
+
+- (void)testEmptyRouteWithoutSessionWithSlash
+{
+  FBRoute *route = [[FBRoute POST:@"/"].withoutSession respond:nil];
+  XCTAssertEqualObjects(route.path, @"/");
 }
 
 @end
