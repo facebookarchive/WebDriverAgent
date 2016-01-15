96de098a252f135739049898fe2ea1935f39a9e8
Marek Cirkos <marekcirkos@users.noreply.github.com>
2016-01-15 23:48:58 +0000

    Revert "HACK: Redefine free() to fix bug in AXRuntime.framework"

--- WebDriverAgent/main.m
+++ WebDriverAgent/main.m
@@ -9,35 +9,8 @@
 
 #import <WebDriverAgentLib/FBUIAWebDriverAgent.h>
 
-#import <dlfcn.h>
-#import <fishhook/fishhook.h>
-
-void my_free(void *ptr);
-static void (*orig_free)(void *);
-
-static void *badFreeCaller;
-
-void my_free(void *ptr) {
-  void *caller = __builtin_return_address(0);
-  if (caller == badFreeCaller) {
-    return;
-  }
-  orig_free(ptr);
-}
-
-
 int main(int argc, char *argv[]) {
   @autoreleasepool {
-    // This is all one terrible hack to deal with a bug in AXRuntime cfAttributedStringUnserialize() where they attempt
-    // to free a pointer which was not allocated.
-    // From doing pointer arithmetic, at the end of this `badFreeCaller` is set to the exact address of the caller which attempts
-    // this within cfAttributedStringUnserialize.
-    // This issue shows up in Xcode 7.2.
-    rebind_symbols((struct rebinding[1]){{"free", my_free, (void *)&orig_free}}, 1);
-    void *AXRuntimeImage = dlopen("/System/Library/PrivateFrameworks/AXRuntime.framework/AXRuntime", RTLD_NOW);
-    void *knownAddress = dlsym(AXRuntimeImage, "AXUnserializeCFType");
-    badFreeCaller = knownAddress + 0x1F69;
-
     [[FBUIAWebDriverAgent sharedAgent] start];
   }
 
