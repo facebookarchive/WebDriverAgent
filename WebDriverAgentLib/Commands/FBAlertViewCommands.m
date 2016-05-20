6210c72019db6cc2103b8f6881bdc2511a5d4cfc
Marek Cirkos <marekc@fb.com>
2016-05-20 09:13:39 -0700

    Added alert endpoints that conform to WebDriver spec
    Summary: Added alert endpoints that conform to WebDriver spec http://www.w3.org/TR/webdriver/#dismiss-alert
    
    Reviewed By: mmmulani
    
    Differential Revision: D3327612
    
    fbshipit-source-id: bc1c1377cb17f5230f046deaf05190e767285b07

--- WebDriverAgentLib/Commands/FBAlertViewCommands.m
+++ WebDriverAgentLib/Commands/FBAlertViewCommands.m
@@ -39,6 +39,10 @@ NSString *const FBUAlertObstructingElementException = @"FBUAlertObstructingEleme
 {
   return
   @[
+    [[FBRoute GET:@"/alert/text"] respondWithTarget:self action:@selector(handleAlertTextCommand:)],
+    [[FBRoute POST:@"/alert/accept"] respondWithTarget:self action:@selector(handleAlertAcceptCommand:)],
+    [[FBRoute POST:@"/alert/dismiss"] respondWithTarget:self action:@selector(handleAlertDismissCommand:)],
+    // TODO: Removed when client is migrated
     [[FBRoute GET:@"/alert_text"] respondWithTarget:self action:@selector(handleAlertTextCommand:)],
     [[FBRoute POST:@"/accept_alert"] respondWithTarget:self action:@selector(handleAlertAcceptCommand:)],
     [[FBRoute POST:@"/dismiss_alert"] respondWithTarget:self action:@selector(handleAlertDismissCommand:)],
