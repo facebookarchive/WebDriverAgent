5096ef00fa3b1aad44a4a2ec3a76e13498bb1c1b
Marek Cirkos <marekc@fb.com>
2016-06-17 01:42:16 -0700

    Tests for FBKeyboard
    Summary: Added simple typing tests
    
    Reviewed By: nqmtuan
    
    Differential Revision: D3444681
    
    fbshipit-source-id: bbe60e05500439ed01423c8828356589de1cca07

---
+++ WebDriverAgentTests/IntegrationTests/FBKeyboardTests.m
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import <XCTest/XCTest.h>
+
+#import "FBKeyboard.h"
+
+@interface FBKeyboardTests : XCTestCase
+@end
+
+@implementation FBKeyboardTests
+
+- (void)setUp
+{
+  [super setUp];
+  [[XCUIApplication new] launch];
+  [[XCUIApplication new].buttons[@"Attributes"] tap];
+}
+
+- (void)testTextTyping
+{
+  NSString *text = @"Happy typing";
+  XCUIElement *textField = [XCUIApplication new].textFields[@"aIdentifier"];
+  [textField tap];
+  NSError *error;
+  XCTAssertTrue([FBKeyboard typeText:text error:&error]);
+  XCTAssertNil(error);
+  XCTAssertEqualObjects(textField.value, text);
+}
+
+- (void)testTypingWithoutKeyboardPresent
+{
+  NSError *error;
+  XCTAssertFalse([FBKeyboard typeText:@"This should fail" error:&error]);
+  XCTAssertNotNil(error);
+}
+
+@end
