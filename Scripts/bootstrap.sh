7ce95d21f990cf32fc623058d5cb74303edfb9e5
Shayan Sayahi <shayansa@amazon.com>
2016-09-09 02:09:19 -0700

    Adding a flag to fetch carthage dependencies from GitHub using SSH credentials
    Summary:
    Addressing https://github.com/facebook/WebDriverAgent/issues/260.
    Closes https://github.com/facebook/WebDriverAgent/pull/274
    
    Differential Revision: D3841000
    
    Pulled By: marekcirkos
    
    fbshipit-source-id: 07e572159f5b0948d2de5b6a6257684807412ced

--- Scripts/bootstrap.sh
+++ Scripts/bootstrap.sh
@@ -38,6 +38,7 @@ function print_usage() {
   echo "Usage:"
   echo $'\t -i Build Inspector bundle'
   echo $'\t -d Fetch & build dependencies'
+  echo $'\t -D Fetch & build dependencies using SSH for downloading GitHub repositories'
   echo $'\t -h print this help'
 }
 
@@ -45,9 +46,10 @@ function fetch_and_build_dependencies() {
   echo -e "${BOLD}Fetching dependencies"
   assert_has_carthage
   if ! cmp -s Cartfile.resolved Carthage/Cartfile.resolved; then
-    carthage bootstrap
+    carthage bootstrap $USE_SSH
     cp Cartfile.resolved Carthage
   fi
+
 }
 
 function build_inspector() {
@@ -73,16 +75,17 @@ function build_inspector() {
   echo "Done"
 }
 
-while getopts " i d h " option; do
+while getopts " i d D h " option; do
   case "$option" in
     i ) BUILD_INSPECTOR=1;;
-    d ) FECHT_DEPS=1;;
+    d ) FETCH_DEPS=1;;
+    D ) FETCH_DEPS=1; USE_SSH="--use-ssh";;
     h ) print_usage; exit 1;;
     *) exit 1 ;;
-    esac
+  esac
 done
 
-if [[ -n ${FECHT_DEPS+x} ]]; then
+if [[ -n ${FETCH_DEPS+x} ]]; then
   fetch_and_build_dependencies
 fi
 
@@ -90,7 +93,7 @@ if [[ -n ${BUILD_INSPECTOR+x} ]]; then
   build_inspector
 fi
 
-if [[ -z ${FECHT_DEPS+x} && -z ${BUILD_INSPECTOR+x} ]]; then
+if [[ -z ${FETCH_DEPS+x} && -z ${BUILD_INSPECTOR+x} ]]; then
   fetch_and_build_dependencies
   build_inspector
 fi
