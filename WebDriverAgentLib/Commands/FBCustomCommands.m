b219cbbb841fb1796d08ae91dac42f9a6b8da54b
Mykola Mokhnach <nick+1@wire.com>
2017-02-16 08:41:27 -0800

    Add separate API call for on-screen keyboard dismissal
    Summary:
    We have _hideKeyboard_ API call in Appium, which is quite hacky for iOS right now. And since there is a special method in _XCUIApplication_ class for exactly such purpose, then the stuff should be implemented in the right way.
    imurchie jlipps FYI
    Closes https://github.com/facebook/WebDriverAgent/pull/473
    
    Differential Revision: D4572349
    
    Pulled By: marekcirkos
    
    fbshipit-source-id: c745b7ea38930a2c56b5aa5031d820384f1a08f3

--- WebDriverAgentLib/Commands/FBCustomCommands.m
+++ WebDriverAgentLib/Commands/FBCustomCommands.m
@@ -18,11 +18,13 @@
 #import "FBResponsePayload.h"
 #import "FBRoute.h"
 #import "FBRouteRequest.h"
+#import "FBRunLoopSpinner.h"
 #import "FBSession.h"
 #import "FBSpringboardApplication.h"
 #import "XCUIApplication+FBHelpers.h"
 #import "XCUIDevice+FBHelpers.h"
 #import "XCUIElement.h"
+#import "XCUIElement+FBIsVisible.h"
 #import "XCUIElementQuery.h"
 
 @implementation FBCustomCommands
@@ -34,6 +36,7 @@
     [[FBRoute POST:@"/timeouts"] respondWithTarget:self action:@selector(handleTimeouts:)],
     [[FBRoute POST:@"/wda/homescreen"].withoutSession respondWithTarget:self action:@selector(handleHomescreenCommand:)],
     [[FBRoute POST:@"/wda/deactivateApp"] respondWithTarget:self action:@selector(handleDeactivateAppCommand:)],
+    [[FBRoute POST:@"/wda/keyboard/dismiss"] respondWithTarget:self action:@selector(handleDismissKeyboardCommand:)],
 
     // TODO: Those endpoints are deprecated and will die soon
     [[FBRoute POST:@"/homescreen"].withoutSession respondWithTarget:self action:@selector(handleHomescreenCommand:)],
@@ -67,6 +70,29 @@
 + (id<FBResponsePayload>)handleTimeouts:(FBRouteRequest *)request
 {
   // This method is intentionally not supported.
+  return FBResponseWithOK();
+}
+
++ (id<FBResponsePayload>)handleDismissKeyboardCommand:(FBRouteRequest *)request
+{
+  [request.session.application dismissKeyboard];
+  NSError *error;
+  NSString *errorDescription = @"The keyboard cannot be dismissed. Try to dismiss it in the way supported by your application under test.";
+  if ([UIDevice.currentDevice userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+    errorDescription = @"The keyboard on iPhone cannot be dismissed because of a known XCTest issue. Try to dismiss it in the way supported by your application under test.";
+  }
+  BOOL isKeyboardNotPresent =
+  [[[[FBRunLoopSpinner new]
+     timeout:5]
+    timeoutErrorMessage:errorDescription]
+   spinUntilTrue:^BOOL{
+     XCUIElement *foundKeyboard = [[FBApplication fb_activeApplication].query descendantsMatchingType:XCUIElementTypeKeyboard].element;
+     return !(foundKeyboard.exists && foundKeyboard.fb_isVisible);
+   }
+   error:&error];
+  if (!isKeyboardNotPresent) {
+    return FBResponseWithError(error);
+  }
   return FBResponseWithOK();
 }
 
