d14cd6de2ed5d450418998667cf1b071ab6b9580
Mykola Mokhnach <nick+1@wire.com>
2017-05-30 11:01:47 +0200

    Address review comments

--- WebDriverAgentLib/FBAlert.m
+++ WebDriverAgentLib/FBAlert.m
@@ -102,7 +102,8 @@ NSString *const FBAlertObstructingElementException = @"FBAlertObstructingElement
   if (resultText.count) {
     return [resultText componentsJoinedByString:@"\n"];
   }
-  return nil;
+  // return null to reflect the fact there is an alert, but it does not contain any text
+  return (id)[NSNull null];
 }
 
 - (NSArray *)buttonLabels
