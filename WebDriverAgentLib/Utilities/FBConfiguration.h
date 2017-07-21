18ca67c5ed9d82c63af8bfa5791f99ce36ac8e6e
Marek Cirkos <marekc@fb.com>
2017-07-21 10:29:29 -0700

    Kill showFakeCollectionViewCells
    Summary:
    This was responsible for showing more elements (including some invisible ones).
    Since we cannot depend on this anymore, it is time to kill it.
    
    Reviewed By: antiarchit
    
    Differential Revision: D5470159
    
    fbshipit-source-id: 808eaeea9c909bbe41f0af1a8c6908e590c2e39f

--- WebDriverAgentLib/Utilities/FBConfiguration.h
+++ WebDriverAgentLib/Utilities/FBConfiguration.h
@@ -32,12 +32,6 @@ NS_ASSUME_NONNULL_BEGIN
 + (NSUInteger)maxTypingFrequency;
 
 /**
- Switch for enabling/disabling reporting fake collection view cells by Accessibility framework.
- If set to YES it will report also invisible cells.
- */
-+ (void)shouldShowFakeCollectionViewCells:(BOOL)showFakeCells;
-
-/**
  The range of ports that the HTTP Server should attempt to bind on launch
  */
 + (NSRange)bindingPortRange;
