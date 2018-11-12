0c5c5e27a507fa62d1a554c1a835c4e077b11694
Pavel Serdziukou <iamshvul@gmail.com>
2018-11-12 23:49:12 +0300

    update copyrights

--- WebDriverAgentLib/Categories/XCUIApplication+FBFocused.h
+++ WebDriverAgentLib/Categories/XCUIApplication+FBFocused.h
@@ -1,10 +1,11 @@
-//
-//  XCUIApplication+FBFocused.h
-//  WebDriverAgentLib_tvOS
-//
-//  Created by Pavel Serdiukov on 8/6/18.
-//  Copyright © 2018 Facebook. All rights reserved.
-//
+/**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #import <XCTest/XCTest.h>
 
